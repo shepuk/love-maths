6f9e082df858f43f00bd7d3826aa5966965f7a02
shepuk <paulshepherd90@live.co.uk>
2022-01-17 18:11:23 +0000

    Added code to tally corret/incorrect score and write it to the dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,8 +47,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Correct :)")
+        incrementScore();
     } else {
-        alert(`you answered ${userAnswer}. The correct answer was ${calculatedAnswer}.`);
+        alert(`You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}.`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -71,12 +73,21 @@ function calculatedCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the current score from the dom and increments it by 1
+ */
 function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**
+ * Gets the current number of wrong answers from the dom and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
